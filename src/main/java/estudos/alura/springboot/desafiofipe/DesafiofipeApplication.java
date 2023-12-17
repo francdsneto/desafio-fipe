e1ea8c6ac015764950c1fb46667576c06264fa1e
Neto <setorti01@comshalom.org>
2023-12-17 01:32:48 -0300

    Atualizando implements da classe DesafioFipeApplication para CommandLineRunner em vez de ApplicationRunner

--- src/main/java/estudos/alura/springboot/desafiofipe/DesafiofipeApplication.java
+++ src/main/java/estudos/alura/springboot/desafiofipe/DesafiofipeApplication.java
@@ -2,20 +2,20 @@ package estudos.alura.springboot.desafiofipe;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafiofipeApplication implements ApplicationRunner {
+public class DesafiofipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafiofipeApplication.class, args);
 	}
 
 	@Override
-	public void run(ApplicationArguments args) throws Exception {
+	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
 		principal.exibeMenu();
 	}
-
 }
